cb829ef0c44e0927b1531f6135b302e7e27f37b4
Attila Reterics <reterics.attila@gmail.com>
2025-10-01 19:11:01 +0200

    chore: fix baseURL issues with build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
   return {
-    base: './',
+    base: mode === 'development' ? './' : (env.VITE_BASENAME || '/'),
     plugins: [react(), tailwindcss()],
     server: {
         host: '0.0.0.0',
